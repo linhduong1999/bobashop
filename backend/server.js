b858ee561f60fc1381c858a538706d02a29b34f3
Linh Duong <duongphuonglinh1999@gmail.com>
2022-03-20 10:53:22 -0500

    Proxy problem

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-const { createSocket } = require('dgram');
+const cors = require('cors')
 const express = require('express')
 const products = require('./data/products')
 //const mongoose = require('mongoose');
 
 const app = express()
+app.use(cors());
 
 app.use(express.json())
 
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const create = (app) => {
-    app.use(
-        ["/api"], 
-        createProxyMiddleware({
-            target: "http://localhost:5001"
-        })
-    );
-}
-module.exports = create;
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5001',
+      changeOrigin: true,
+    })
+  );
+};
